a07d763f823d03751ababb4a5306e95eb72f4573
robertoahc85 <robertoahc85@gmail.com>
2024-07-25 11:30:30 -0600

    cambio jquery

--- HTML/seccion_10/js/formulario.js
+++ HTML/seccion_10/js/formulario.js
@@ -16,13 +16,21 @@ $(document).ready(function () {
         if (username === "") {
             isValid = false;
             $("#username").after('<span class="error" > El nombre de usuarios es obligatorio</span>');
+        }
+
+        // Validación del correo electrónico
+        const email = $('#email').val().trim();
+        if (email === '') {
+            isValid = false;
+            $('#email').after('<span class="error">El correo electrónico es obligatorio.</span>');
         } else {
-            const emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+            const emailPattern = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
             if (!emailPattern.test(email)) {
                 isValid = false;
-                $("#email").after('<span class="error"> Ingrese un email valido </span>');
+                $('#email').after('<span class="error">Ingrese un correo electrónico válido.</span>');
             }
         }
+
 
         //validacion de la contraseña
         const password = $("#password").val().trim();
@@ -33,6 +41,7 @@ $(document).ready(function () {
         if (isValid) {
             successMessage = '<p class="success"> Formulario enviado exitosamente</p>'
             $("#formMessage").html(successMessage);
+            //enviar al formulario
         } else {
             errorMessage = '<p class="error"> Por favor corrige los errores en el formulario</p>'
             $("#formMessage").html(errorMessage);
